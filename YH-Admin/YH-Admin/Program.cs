fa61f8181ec5200da42c92a042245e0ba5653ce1
OHolvik <oscar.holvik@hotmail.com>
2016-04-18 12:36:09 +0200

    New project

---
+++ YH-Admin/YH-Admin/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] usernamePassword;
+
+
+        string userName;
+        string password;
+        Console.WriteLine("Användarnamn:");
+        userName = Console.ReadLine();
+        Console.WriteLine("Lösenord:");
+        password = Console.ReadLine();
+
+        string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Petter\Documents\Login.txt");
+
+        foreach (string line in lines)
+        {
+
+            usernamePassword = line.Split(',');
+            if (usernamePassword[0] == userName)
+            {
+                Console.WriteLine("Hittade användare... kontrollerar lösenord..");
+                if (usernamePassword[1] == password)
+                {
+                    Console.WriteLine("Lösesnord korrekt... loggar in användare.. ");
+                }
+
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
