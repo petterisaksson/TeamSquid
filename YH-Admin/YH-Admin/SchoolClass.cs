a4d1bf57edfbd1aed28a464f187a619a540c2fe1
Johnson Ho <m97_jno@hotmail.com>
2016-04-19 16:43:25 +0200

    Nu är klasserna SchoolClass och Student med.

---
+++ YH-Admin/YH-Admin/SchoolClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin
+{
+    public class SchoolClass
+    {
+        public int ClassId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<int> StudentIds { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public SchoolClass(string line)
+        {
+
+            try
+            {
+                var splits = line.Split(' ');
+                ClassId = int.Parse(splits[0]);
+                Name = splits[1];
+                var nums = splits[2].Split(',');
+                StudentIds = new List<int>();
+                foreach (var n in nums)
+                {
+                    StudentIds.Add(int.Parse(n));
+                }
+                StartDate = DateTime.ParseExact(splits[3], "yyyymmdd", null);
+                EndDate = DateTime.ParseExact(splits[4], "yyyymmdd", null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in creating SchoolClass: " + ex);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return ClassId + ": " + Name + ", studIds: " + GetStudentIds() + "; " + GetStartDate() + "->" + GetEndDate();
+        }
+
+        string GetStudentIds()
+        {
+            string str = "";
+            foreach (var id in StudentIds)
+            {
+                str += id + ",";
+            }
+            return str.Remove(str.Length - 1);
+        }
+
+        string GetStartDate()
+        {
+            return StartDate.ToString("yyyymmdd");
+        }
+
+        string GetEndDate()
+        {
+            return EndDate.ToString("yyyymmdd");
+        }
+    }
+}
---
+++ YH-Admin/YH-Admin/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public Student(string line)
+        {
+            try
+            {
+                var splits = line.Split(' ');
+                StudentId = int.Parse(splits[0]);
+                FirstName = splits[1];
+                LastName = splits[2];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in creating Student: " + ex);
+            }
+        }
+
+        public override string ToString()
+        {
+            return StudentId + " " + FirstName + " " + LastName;
+        }
+    }
+}
