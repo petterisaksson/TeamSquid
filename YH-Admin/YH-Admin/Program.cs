c8179ebb3b997b824590f21a03af4f67be074a69
Johnson Ho <m97_jno@hotmail.com>
2016-04-19 14:43:11 +0200

    La till klassen SchoolClass och Student La till metod för att läsa in från students.txt La till metod för att hämta studenter som går en viss klass

--- YH-Admin/YH-Admin/Program.cs
+++ YH-Admin/YH-Admin/Program.cs
@@ -5,11 +5,18 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YH_Admin;
 
 class Program
 {
     static void Main(string[] args)
     {
+        School mySchool = new School();
+
+        mySchool.LoadData();
+
+
+
         string soluPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
         var userPath = Path.Combine(soluPath, @"DataFiles\user.txt");
         Console.WriteLine(userPath);
