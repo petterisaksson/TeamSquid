5ee9b2561892acd038d7c4731de0871e35d17c18
Nikki Öhrn <ohrn.nikki@outlook.com>
2016-04-20 11:20:31 +0200

    Class ConsoleOutput

---
+++ YH-Admin/YH-Admin/ConsoleOutput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin
+{
+    class ConsoleOutput
+    {
+        public void Menu()
+        {
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": Console.WriteLine("Utbildningar");
+                    break;
+                case "2": Console.WriteLine("Klasser");
+                    break;
+                case "3": Console.WriteLine("Kurser");
+                    break;
+                case "4": Console.WriteLine("Undervisare");
+                    break;
+                case "5": Console.WriteLine("Studerande");
+                    break;
+            }
+        }
+    }
+}
