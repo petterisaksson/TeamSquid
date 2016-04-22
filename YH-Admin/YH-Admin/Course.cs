7b72047db65a9c71bc433b567243c58e4ff0b10f
Oscar Holvik <oscar.holvik@hotmail.com>
2016-04-22 11:32:24 +0200

    Added class Course

---
+++ YH-Admin/YH-Admin/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Course(int courseId, string courseName, DateTime startDate, DateTime endDate)
+        {
+            courseId = CourseId;
+            courseName = CourseName;
+            startDate = StartDate;
+            endDate = EndDate;
+
+        }
+
+    }
+}
--- YH-Admin/YH-Admin/Program.cs
+++ YH-Admin/YH-Admin/Program.cs
@@ -12,6 +12,7 @@ class Program
     static void Main(string[] args)
     {
         School mySchool = new School();
+        
 
         mySchool.LoadData();
 
