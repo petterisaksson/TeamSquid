7f8d6a42427f46a35fdbc97feefab80293ab2bf2
Johnson Ho <m97_jno@hotmail.com>
2016-04-24 14:53:33 +0200

    added classcourse

---
+++ YH-Admin/YH-Admin/Model/ClassCourse.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin.Model
+{
+    public class ClassCourse
+    {
+        public int ClassCourseId { get; set; }
+
+        public int ClassId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool Finished { get { return EndDate < DateTime.Today; } }
+
+        public ClassCourse(int classCourseId, int classId, int courseId, DateTime startDate, DateTime endDate)
+        {
+            ClassCourseId = ClassCourseId;
+            ClassId = classId;
+            CourseId = courseId;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        /// <summary>
+        /// Get the start date of this course as a string.
+        /// </summary>
+        /// <returns></returns>
+        string GetStartDate()
+        {
+            return StartDate.ToString("yyyyMMdd");
+        }
+
+        /// <summary>
+        /// Get the end date of this course as a string.
+        /// </summary>
+        /// <returns></returns>
+        string GetEndDate()
+        {
+            return EndDate.ToString("yyyyMMdd");
+        }
+
+        /// <summary>
+        /// Default string output.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return ClassCourseId + " " + ClassId + " " + CourseId + "; " + GetStartDate() + "->" + GetEndDate();
+
+        }
+
+        public string ShowCourseStatus()
+        {
+            return GetStartDate() + "->" + GetEndDate() + " Status: " + (Finished ? "Avslutat" : "Pågående" );
+        }
+
+    }
+}
--- YH-Admin/YH-Admin/Program.cs
+++ YH-Admin/YH-Admin/Program.cs
@@ -12,28 +12,7 @@ class Program
 
         ConsoleController controller = new ConsoleController(school, output);
         controller.ShowMainMenu();
-
-        //#region TestRegion
-
-        //// Test code: GetClasses in a certain education.
-        //Console.WriteLine("GetClasses in education: 0");
-        //var classes = mySchool.GetClasses(0);
-        //foreach (var s in classes)
-        //{
-        //    Console.WriteLine(s.ShowClassStatus());
-        //}
-
-        //// Test code: GetStudents in a certain class.
-        //Console.WriteLine("GetStudents in class: 2");
-        //var students = mySchool.GetStudents(2);
-        //foreach (var s in students)
-        //{
-        //    Console.WriteLine(s);
-        //}
-
-        //#endregion //TestRegion
-
-
+ 
     }
 
 }
