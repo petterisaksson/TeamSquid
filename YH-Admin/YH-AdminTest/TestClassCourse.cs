e764de40f723edbb4f45acdb7767f98eebdcd8cb
Nikki Öhrn <ohrn.nikki@outlook.com>
2016-04-26 10:42:41 +0200

    test course + classcourse

--- YH-Admin/YH-AdminTest/TestClassCourse.cs
+++ YH-Admin/YH-AdminTest/TestClassCourse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YH_Admin.Model;
 
 namespace YH_AdminTest
 {
@@ -9,6 +10,16 @@ namespace YH_AdminTest
         [TestMethod]
         public void TestMethod1()
         {
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            ClassCourse cc = new ClassCourse(1, 2, 3, new DateTime(2016, 01, 01), new DateTime(2016,02,02));
+            string expected = $"ClassCourseId: 1; CourseId: 1; ClassId: 2; 20160101; 20160202";
+
+            var actual = $"ClassCourseId: 1; CourseId: 1; ClassId: 2; 20160101; 20160202";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
