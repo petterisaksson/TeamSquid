c668678d61c537c5f781bf650f8b09071d2cb1df
Petter <Petter@ISAKSSON>
2016-04-26 14:18:46 +0200

    asd

---
+++ YH-Admin/YH-AdminTest/GetStudents1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YH_Admin.Model;
+
+namespace YH_AdminTest
+{
+    [TestClass]
+    public class GetStudents1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SchoolClass n = new SchoolClass(1, "name", 2, new DateTime(2013,09,01), new DateTime(2015,05,30));
+            string expected = $"classId: 0 SU13 0 20130901 20150530"
+
+        }
+    }
+}
