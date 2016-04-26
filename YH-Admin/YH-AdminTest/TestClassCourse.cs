b6c898d6b1e43292d395bc8b65551a5a98e4325c
Mats <mats@drakneid.se>
2016-04-26 10:43:06 +0200

    TestMetod

--- YH-Admin/YH-AdminTest/TestClassCourse.cs
+++ YH-Admin/YH-AdminTest/TestClassCourse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YH_Admin.Model;
 
 namespace YH_AdminTest
 {
@@ -7,8 +8,22 @@ namespace YH_AdminTest
     public class TestClassCourse
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetStartDate()
         {
+            //Arange
+            ClassCourse gsd = new ClassCourse(1, 2, 3, new DateTime(2016,04,26), new DateTime(2016,02,02));
+            string expected = $"classCourseId:1; classId:2; courseId:3; 20160426; 20160202;";
+
+            //Act
+            var actual = $"classCourseId:1; classId:2; courseId:3; 20160426; 20160202;";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
         }
     }
+    
+
+
 }
