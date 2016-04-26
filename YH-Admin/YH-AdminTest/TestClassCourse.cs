69bd5e859c0d06c98c4d771e7189cb55c44babba
joho77 <joho77@gmail.com>
2016-04-26 10:41:38 +0200

    Test for GetEndDate in ClassCourse

--- YH-Admin/YH-AdminTest/TestClassCourse.cs
+++ YH-Admin/YH-AdminTest/TestClassCourse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YH_Admin.Model;
 
 namespace YH_AdminTest
 {
@@ -7,8 +8,14 @@ namespace YH_AdminTest
     public class TestClassCourse
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetEndDate()
         {
+            ClassCourse cc = new ClassCourse(0, 0, 0, new DateTime(2012, 3, 11), new DateTime(2013, 4, 12));
+            string expected = "20130412";
+
+            string actual = cc.GetEndDate();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
