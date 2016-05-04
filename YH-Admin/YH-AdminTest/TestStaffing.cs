5be8ca0a2ea9bee22c2b37b6c4efcd8ec6444912
Petter <Petter@ISAKSSON>
2016-05-04 10:41:32 +0200

    teststaffing added

---
+++ YH-Admin/YH-AdminTest/TestStaffing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YH_AdminTest
+{
+    [TestClass]
+    public class TestStaffing
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
