ea8d0cb2deff91e1b3b8cd2eea3b80e3427f006c
Mats <mats@drakneid.se>
2016-05-06 14:02:55 +0200

    StaffingClass

--- YH-Admin/YH-Admin/Model/Staffing.cs
+++ YH-Admin/YH-Admin/Model/Staffing.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace YH_Admin.Model
 {
-    class Staffing
+    public class Staffing
     {
+        public int StaffingId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public String Name { get { return $"{FirstName} {LastName}"; } }
+
+        
+
+
+
+        public Staffing(int staffingId, string firstName, string lastName)
+        {
+            StaffingId = staffingId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Staffing sf = obj as Staffing;
+            if ((System.Object) sf == null)
+                return false;
+
+            return (StaffingId == sf.StaffingId) && (Name == sf.Name);
+        }
     }
 }
+
+    
---
+++ YH-Admin/YH-Admin/Model/StaffingCourse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YH_Admin.Model
+{
+    class StaffingCourse
+    {
+        public int StaffingCourseId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public int StaffingId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        //okpublic string None { get; set; }
+
+        public string StartDateString { get { return StartDate.ToString("yyyyMMdd"); } }
+
+        public bool IsNotAssign { get; set; }
+
+        public string Status { get { return IsNotAssign ? "Tillsättas" : "Tillsatt"; } }
+    }
+}
